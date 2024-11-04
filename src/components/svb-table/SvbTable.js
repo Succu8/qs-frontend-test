998c75f7dcc86cc3193776d6834756d456cd6b2b
Sayan <sayan.almaz40@gmail.com>
2024-11-05 03:34:01 +0500

    feat(): started logic implementation

--- src/components/svb-table/SvbTable.js
+++ src/components/svb-table/SvbTable.js
@@ -2,20 +2,26 @@ import './styles/svb-table.scss'
 
 export default class SvbTable {
 
+  _activeRow = '';
+
   renderTable(tableData) {
     const {settings, columns, rows} = tableData;
 
     const table = SvbTable.createElement('table', 'doc-list-table', 'svb-table');
     const thead = document.createElement('thead');
     const tbody = document.createElement('tbody');
+    const tfoot = document.createElement('tfoot');
 
+    // THEAD && TFOOT
     const headerRow = document.createElement('tr');
+    const footerRow = document.createElement('tr');
 
     // header order column
     const numberTh = document.createElement('th');
     numberTh.textContent = '№';
     numberTh.dataset.name = 'order';
     headerRow.appendChild(numberTh);
+    footerRow.appendChild(document.createElement('th'));
 
     columns.forEach((column) => {
       if (column === 'uuid') return;
@@ -24,15 +30,23 @@ export default class SvbTable {
       th.textContent = settings[column]?.represent || column;
       th.dataset.name = column;
       th.style.minWidth = settings[column]?.columnWidth + 'px' || 'auto';
+      th.style.maxWidth = settings[column]?.columnWidth + 'px' || 'auto';
       th.style.textAlign = settings[column]?.textAlign;
 
       headerRow.appendChild(th);
+      footerRow.appendChild(document.createElement('th'));
     });
-    thead.appendChild(headerRow);
 
+    thead.appendChild(headerRow);
+    tfoot.appendChild(footerRow);
+
+
+    // TBODY
     rows.forEach((row, i) => {
       const tr = document.createElement('tr');
       tr.dataset.uuid = row[0];
+
+      tr.addEventListener('click', (event) => this.handleRowClick(event, row[0], rows, tbody));
 
       // row order column
       const numberTd = document.createElement('td');
@@ -46,6 +60,7 @@ export default class SvbTable {
         const td = document.createElement('td');
         td.dataset.name = columns[j];
         td.style.minWidth = settings[columns[j]]?.columnWidth + 'px' || 'auto';
+        td.style.maxWidth = settings[columns[j]]?.columnWidth + 'px' || 'auto';
         td.style.textAlign = settings[columns[j]]?.textAlign;
 
         if (typeof cellData === 'object' && cellData !== null && 'r' in cellData) {
@@ -62,6 +77,7 @@ export default class SvbTable {
 
     table.appendChild(thead);
     table.appendChild(tbody);
+    table.appendChild(tfoot);
 
     return table;
   }
@@ -87,5 +103,41 @@ export default class SvbTable {
     if (innerHTML) element.innerHTML = innerHTML;
 
     return element;
+  }
+
+  handleRowClick(event, rowId, rows, tbody) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    if (this._activeRow) {
+      const prevActiveRowIdx = rows.findIndex(x => x[0] === this._activeRow);
+      const prevActiveRowEl = tbody.rows.item(prevActiveRowIdx);
+      if (prevActiveRowEl && prevActiveRowEl.classList.contains('active-tr')) {
+        prevActiveRowEl.classList.remove('active-tr');
+      }
+    }
+
+    this._activeRow = rowId;
+    const currentRow = event.currentTarget;
+    currentRow.classList.add('active-tr');
+  }
+
+  loadRows() {
+  }
+
+  addRow(rowData) {
+  }
+
+  removeRow() {
+  }
+
+  getActiveRow() {
+    return this._activeRow;
+  }
+
+  setValue(args) {
+  }
+
+  getValue() {
   }
 }
